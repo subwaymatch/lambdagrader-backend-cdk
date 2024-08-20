9e493156a39bb36f06c44935f9489dcb06378601
Ye Joo Park <subwaymatch@gmail.com>
2024-08-20 15:39:11 -0500

    Initial commit

---
+++ tests/unit/test_lambdagrader_cdk_stack.py
@@ -0,0 +1,15 @@
+import aws_cdk as core
+import aws_cdk.assertions as assertions
+
+from lambdagrader_cdk.lambdagrader_cdk_stack import LambdagraderCdkStack
+
+# example tests. To run these tests, uncomment this file along with the example
+# resource in lambdagrader_cdk/lambdagrader_cdk_stack.py
+def test_sqs_queue_created():
+    app = core.App()
+    stack = LambdagraderCdkStack(app, "lambdagrader-cdk")
+    template = assertions.Template.from_stack(stack)
+
+#     template.has_resource_properties("AWS::SQS::Queue", {
+#         "VisibilityTimeout": 300
+#     })
